8a342f453028442b48ebe2c2f3f3149962eda82d
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-11-18 22:38:52 +0700

    Proses menu yang nanti dipilih menggunakan switch case

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -21,7 +21,7 @@ namespace double_linked_list
         Node START;
 
         //constructor
-        public void addNote()
+        public void addNode()
         {
             int nim;
             string nm;
@@ -177,6 +177,73 @@ namespace double_linked_list
                     Console.WriteLine("6. Exit\n");
                     Console.Write("Enter your choice (1-6): ");
                     char ch = Convert.ToChar(Console.ReadLine());
+
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.addNode();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.listEmpty())
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Console.WriteLine("\nEnter the roll number of the student" + "whose record is to be deleted: ");
+                                int rollNo = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine();
+                                if (obj.delNode(rollNo) == false)
+                                    Console.WriteLine("Record not found");
+                                else
+                                    Console.WriteLine("Record with roll number" + rollNo + "deleted \n");
+                            }
+                            break;
+                        case '3':
+                            {
+                                obj.ascending();
+                            }
+                            break;
+                        case '4':
+                            {
+                                obj.descending();
+                            }
+                            break;
+                        case '5':
+                            {
+                                if (obj.listEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Node prev, curr;
+                                prev = curr = null;
+                                Console.Write("\nEnter the roll number of student whose record you want to search: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.Search(num, ref prev, ref curr) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("\nRecord found");
+                                    Console.WriteLine("\nRoll number: " + curr.noMhs);
+                                    Console.WriteLine("\nName: " + curr.name);
+                                }
+
+                            }
+                            break;
+                        case '6':
+                            {
+                                return;
+                            }
+                            break;
+                        default:
+                            {
+                                Console.WriteLine("\nInvalid option");
+                            }
+                            break;
+                    }
                 }
                 catch (Exception e)
                 {
