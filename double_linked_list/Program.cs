c61762a4e1c3b47d17cfb1f4d46c3ef33d11823b
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-11-18 16:50:20 +0700

    buat class node dan definisikan variabelnya

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -1,5 +1,18 @@
 ﻿using System;
 namespace double_linked_list
 {
+    class node
+    {
+        /*Node class represent the node of doubly linked list
+         * It consist of the information part and links to 
+         * its succedding and preceeding
+         * in terms of next and previous */
 
+        public int noMhs;
+        public string name;
+        //point to the succeding node
+        public node next;
+        //point to the preceeding node
+        public node prev;
+    }
 }
