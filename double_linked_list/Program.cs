414f7eabdec9fb67adcc09e2c640a0403e98859c
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-11-18 22:20:21 +0700

    buat tampilan menu menggunakan try catch exception yang berada didalam while

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -164,6 +164,25 @@ namespace double_linked_list
         static void Main(string[] args)
         {
             DoubleLinkedList obj = new DoubleLinkedList();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. Add a record to the list");
+                    Console.WriteLine("2. Delete a record from the list");
+                    Console.WriteLine("3. View all records in the ascending order of roll numbers");
+                    Console.WriteLine("4. View all records in the descending order of roll numbers");
+                    Console.WriteLine("5. Search for a record in the list");
+                    Console.WriteLine("6. Exit\n");
+                    Console.Write("Enter your choice (1-6): ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine ("Check for the values entered.");
+                }
+            }
         }
 
 
